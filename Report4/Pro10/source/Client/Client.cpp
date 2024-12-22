399842c924edcd2f84c060ab655598d25ecbcb88
swatper <swatper@swatper>
2024-12-22 17:18:34 +0900

    Sending File Complete

--- Report4/Pro10/source/Client/Client.cpp
+++ Report4/Pro10/source/Client/Client.cpp
@@ -41,23 +41,30 @@ void Client::on_pushButton_clicked(){
             //파일 열기 
             if (file.open(QIODevice::ReadOnly)) {
                 QByteArray fData = file.readAll();
-                QString fPath = fileName;
-                QFileInfo fileInfo(fPath);
-                QString fName = fileInfo.fileName();
                 file.close();
-                QByteArray result = "\n" +fName.toUtf8() + "\n" + fData;
-                send_flag = writeData(result);
-                if (!send_flag) {
+                QString header = QString("FILE:%1:%2\n\n")
+                      .arg(QFileInfo(fileName).fileName())
+                      .arg(fData.size());
+                QByteArray data = header.toUtf8() + fData;
+                send_flag = writeData(data);
+                if (send_flag) {
+                    ui->textEdit->insertPlainText("You sent a file: " + fileName + "\n");
+                } 
+                else {
                     ui->textEdit->insertPlainText("File send fail\n");
                 }
             } 
         }
         if(!sendData.isEmpty()){
-            send_flag = writeData(sendData.toStdString().c_str());
-            ui->lineEdit->setText("");
-            if(!send_flag){
+            QString message = QString("MESSAGE:%1\n").arg(sendData);
+            send_flag = writeData(message.toUtf8());
+            if (send_flag) {
+                // 자신의 화면에 직접 메시지 표시
+                ui->textEdit->insertPlainText("You: " + sendData + "\n");
+            } else {
                 ui->textEdit->insertPlainText("Message send fail\n");
             }
+            ui->lineEdit->setText("");
         }
         ui->lineEdit->setEnabled(true);
         ui->label->setText("File is Empty");
@@ -91,12 +98,66 @@ void Client::on_pushButton3_clicked(){
     }
 }
 
-void Client::readyRead(){
-    QTcpSocket *socket = static_cast<QTcpSocket *>(sender());
-    while (socket->bytesAvailable() > 0){
-        ui->textEdit->insertPlainText(QString(socket -> readAll()) + "\r\n");
+void Client::readyRead() {
+    static QByteArray buffer;
+
+    while (socket->bytesAvailable() > 0) {
+        buffer.append(socket->readAll());
+
+        // 파일 데이터 처리
+        if (buffer.startsWith("FILE:")) {
+            int headerEnd = buffer.indexOf("\n\n");
+            if (headerEnd != -1) {
+                QByteArray header = buffer.mid(0, headerEnd);
+                QList<QByteArray> headerParts = header.split(':');
+                if (headerParts.size() < 4) { // FILE:Client <ID>:<FileName>:<FileSize>
+                    ui->textEdit->insertPlainText("Invalid file header format\n");
+                    buffer.clear();
+                    return;
+                }
+
+                QString clientId = headerParts[1].trimmed();  // "Client 1"에서 ID 추출
+                QString fileName = QString(headerParts[2]);   // 받은 파일명
+                int fileSize = headerParts[3].toInt();        // 파일 크기
+
+                QByteArray fileData = buffer.mid(headerEnd + 2);
+                if (fileData.size() >= fileSize) { // 충분한 데이터를 수신한 경우
+                    // 화면에 출력
+                    ui->textEdit->insertPlainText("File received: received_" + clientId + ": " + fileName + "\n");
+
+                    // 파일을 로컬에 저장 (received_<FileName>)
+                    QFile file("received_" + fileName);
+                    if (file.open(QIODevice::WriteOnly)) {
+                        file.write(fileData.left(fileSize)); // 파일 데이터를 저장
+                        file.close();
+                        ui->textEdit->insertPlainText("File saved as: received_" + fileName + "\n");
+                    } else {
+                        ui->textEdit->insertPlainText("Failed to save file: received_" + fileName + "\n");
+                    }
+
+                    buffer.remove(0, headerEnd + 2 + fileSize);
+                } else {
+                    // 파일 데이터가 아직 부족하면 대기
+                    return;
+                }
+            }
+        }
+        // 메시지 데이터 처리
+        else if (buffer.startsWith("MESSAGE:")) {
+            int messageEnd = buffer.indexOf("\n");
+            if (messageEnd != -1) {
+                QString message = QString(buffer.mid(8, messageEnd - 8).trimmed());
+                ui->textEdit->insertPlainText(message + "\n");
+
+                buffer.remove(0, messageEnd + 1);
+            }
+        } else {
+            ui->textEdit->insertPlainText("Unknown data format, clearing buffer\n");
+            buffer.clear();
+        }
     }
 }
+
 int main(int argc, char **argv){
     QApplication app(argc, argv);
     Client *window = new Client();
